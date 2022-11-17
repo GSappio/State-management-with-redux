03ef6c975174f7ca0469aa69abfafbfe86219fa5
Gabriel <gabriel.sappio02@gmail.com>
2022-11-17 08:44:39 -0300

    Invoking action creator #01

--- src/components/Intervalo.jsx
+++ src/components/Intervalo.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import Card from './Card'
+import { alterarNumeroMinimo } from '../store/actions/numeros'
 
 function Intervalo(props) {
     const {min, max} = props;
@@ -29,4 +30,14 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps) (Intervalo)
+function mapActionCreatorsToProp(dispatch) {
+    return {
+        alterarMinimo(novoNumero) {
+            // Action Creator -> Action
+            const action = alterarNumeroMinimo(novoNumero)
+            dispatch(action)
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapActionCreatorsToProp) (Intervalo)
